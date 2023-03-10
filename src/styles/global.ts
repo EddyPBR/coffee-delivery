500ee1ba1dcf00fde972585951ca21f97eaebf6e
EddyPBR <edvaldojunior1310@gmail.com>
2023-03-09 21:24:02 -0300

    :sparkles: add theme provider & default theme in all application

---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme["purple"]};
+  }
+
+  body {
+    background: ${(props) => props.theme["background"]};
+    color: ${(props) => props.theme["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body {
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    line-height: 1.3;
+    font-weight: 400;
+  }
+
+  input, textarea, button {
+    font-size: 0.875rem;
+  }
+  
+  h1, h2, h3, h4, h5, h6, strong {
+    font-family: "Baloo 2", cursive;
+    font-size: 1.25rem;
+    font-weight: 700;
+    color: ${(props) => props.theme["base-title"]};
+  }
+`;
