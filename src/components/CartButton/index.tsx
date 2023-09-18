cd56c1c201c796078df1cc466a975dcf4c1ae35f
EddyPBR <edvaldojunior1310@gmail.com>
2023-09-18 18:03:20 -0300

    :sparkles: add CardButton in secondary color

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,14 +1,12 @@
-import { ButtonContainer } from "./styles";
+import { ButtonContainer, IButtonContainerProps } from "./styles";
 
 import { ShoppingCart } from "@phosphor-icons/react";
 
-export interface ICartButtonProps {
-  quantity?: number;
-}
+export interface ICartButtonProps extends IButtonContainerProps {};
 
-export function CartButton({ quantity }: ICartButtonProps) {
+export function CartButton({ quantity, secondary = false }: ICartButtonProps) {
   return (
-    <ButtonContainer quantity={quantity}>
+    <ButtonContainer quantity={quantity} secondary={secondary}>
       <ShoppingCart weight="fill" />
     </ButtonContainer>
   );
--- src/components/CartButton/styles.ts
+++ src/components/CartButton/styles.ts
@@ -1,25 +1,31 @@
 import styled from "styled-components";
 
-interface IButtonContainerProps {
+export interface IButtonContainerProps {
   quantity?: number;
+  secondary?: boolean;
 }
 
 export const ButtonContainer = styled.button<IButtonContainerProps>`
-  background-color: ${(props) => props.theme["yellow-light"]};
+  background-color: ${(props) => props.secondary ? props.theme["purple"] : props.theme["yellow-light"]};
   padding: 0.5rem;
 
   border-radius: 0.375rem; // 6px
   display: inline-flex;
 
   position: relative;
+  transition: background ease-in-out .2s;
 
   svg {
-    color: ${(props) => props.theme["yellow-dark"]};
+    color: ${(props) => props.secondary ? props.theme["base-card"] : props.theme["yellow-dark"]};
     font-size: 1.375rem; // 22px
   }
 
+  &:hover {
+    background-color: ${(props) => props.secondary ? props.theme["purple-dark"] : undefined};
+  }
+
   &:focus {
-    box-shadow: 0 0 0 2px ${(props) => props.theme["yellow-dark"]};
+    box-shadow: 0 0 0 2px ${(props) => props.secondary ? props.theme["purple-dark"] : props.theme["yellow-dark"]};
   }
 
   &::after {
@@ -32,7 +38,7 @@ export const ButtonContainer = styled.button<IButtonContainerProps>`
     height: 1.25rem; // 22px
     width: 1.25rem; // 22px
 
-    background-color: ${(props) => props.theme["yellow-dark"]};
+    background-color: ${(props) => props.secondary ? props.theme["purple-dark"] : props.theme["yellow-dark"]};
     color: ${(props) => props.theme.white};
 
     font-size: 0.75rem; // 12px
@@ -46,6 +52,6 @@ export const ButtonContainer = styled.button<IButtonContainerProps>`
   }
 
   &:focus::after {
-    box-shadow: 0 0 0 2px ${(props) => props.theme["yellow-dark"]};
+    box-shadow: 0 0 0 2px ${(props) => props.secondary ? props.theme["purple-dark"] : props.theme["yellow-dark"]};
   }
 `;
