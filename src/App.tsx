d82ca0ec236a9c4de7b61aaa71114e6656419245
EddyPBR <edvaldojunior1310@gmail.com>
2023-09-21 17:54:21 -0300

    :sparkles: enable Router in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <h1>Coffee Delivery</h1>
-        <p>Hello world</p>
-      </div>
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   );
